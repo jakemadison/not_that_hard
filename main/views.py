c9f5b200ac466a47048917764cb3db006155093a
Jake Madison <jakemadison@gmail.com>
2015-01-02 11:36:09 -0800

    main template created

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 # Create your views here.
+
+
+def index(request):
+    context = RequestContext(request)
+
+    context_dict = {'boldmessage': "Testing.....1,2,3"}
+
+    return render_to_response('index.html', context_dict, context)
