e03897b6978aba23474f6663dc762e5b8dac8244
Jake Madison <jakemadison@gmail.com>
2015-01-04 13:04:14 -0800

    d3'n.

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@
  * Created by jmadison on 1/2/15.
  */
 
-
+// get some data:
 $.get('get_historical_data', function(result) {
 
     console.log('received a result!  woo!', result);
@@ -28,18 +28,88 @@ $.get('get_historical_data', function(result) {
 
         console.log('this row: ', this_row);
         test_table.append(this_row);
+
+
     }
 
 
+    create_donut(result.data);
+
 });
 
-var vis = d3.select('.chart');
 
-var arc = d3.svg.arc().innerRadius(50)
-    .outerRadius(100)
-    .startAngle(0)
-    .endAngle(1.5*Math.PI);
+function create_donut(data) {
 
-vis.append("path")
-    .attr("d", arc)
-    .attr("transform", "translate(300,200)");
+    console.log('create donut... ');
+
+    var number_donuts = data.length;
+    var number_of_segments = 4;
+    var colour_array = ["#AA8888", "#88BB88", "#8888CC", "#AA88CC"];
+
+
+    var radius = 74;
+    var padding = 10;
+
+
+    var vis = d3.select('.chart');
+
+    // map 0 - 100 on to 0 - 2*Pi:
+    var arcScale = d3.scale.linear().domain([0, 100]).range([0, 2*Math.PI]);
+
+    var arc = d3.svg.arc().innerRadius(radius - 30)
+        .outerRadius(radius)
+        .startAngle(function(d, i) {
+            return arcScale(i*25);
+        })
+        .endAngle(function(d, i) {
+            return arcScale(25 + (i*25));
+        });
+
+      var color = d3.scale.ordinal()
+        .domain(colour_array)
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+
+    vis.selectAll("path").data(data).enter()
+        .append("path")
+        .attr("d", arc)
+
+        //.style("fill", function(d, i) {
+        //    return colour_array[i];
+        //})
+
+        .style("fill", color)
+        .attr("transform", "translate(300,200)");
+
+    console.log('donut done.');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
