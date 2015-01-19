9dd3b8b8db8a072315b642f0414b56e3585d7aa3
Jake Madison <jakemadison@gmail.com>
2015-01-19 08:47:56 -0800

    hokay, front end is somewhat exploded, but I think this is the right track.

--- main/controller.py
+++ main/controller.py
@@ -21,70 +21,6 @@ def construct_data_array():
     return historical_data_array
 
 
-def construct_data_array_new():
-
-    # should be in the form: array = [{day: date, wealth: [1, 2], health}]
-
-    parsed_data_array = []
-
-    # crap, this needs to be an outer join on day
-    historical_data = models.Event.objects.all().select_related().order_by('day_link__date')
-
-    # for each in historical_data[0].events:
-    #     print(each)
-
-    categories = ['arts', 'smarts', 'wealth', 'health']
-
-    parsed_datum = {}
-
-    for each_event in historical_data:
-
-        print(each_event.major_event, each_event.day_link.date,
-              each_event.category, each_event.name, end=' ')
-
-        if 'day' not in parsed_datum:
-            print('.....new element, no day in key.')
-            parsed_datum['day'] = datetime.strftime(each_event.day_link.date, '%b %d')
-            parsed_datum['notes'] = each_event.day_link.notes
-
-        elif parsed_datum['day'] != datetime.strftime(each_event.day_link.date, '%b %d'):
-
-            print('....day is a key in the datum, but does not match this event.')
-
-            print('\n ----> this datum is complete, pushing to stack: {0}'.format(parsed_datum))
-
-            parsed_data_array.append(parsed_datum)
-            parsed_datum = {'day': datetime.strftime(each_event.day_link.date, '%b %d'),
-                            str(each_event.category): [str(each_event.name)]}
-            continue
-
-        if str(each_event.category) in parsed_datum:
-
-            print('....this category exists in the datum, appending.')
-
-            parsed_datum[str(each_event.category)].append(str(each_event.name))
-
-        else:
-            if each_event.major_event:
-                print('i recognized this as a major event!')
-                parsed_datum[str(each_event.category)] = [str(each_event.name), str(each_event.name)]
-                print('datum after adding: {0}'.format(parsed_datum))
-
-            else:
-                print('....this day exists, but an unknown category.  Creating.')
-                parsed_datum[str(each_event.category)] = [str(each_event.name)]
-
-    # loop is done!
-
-    parsed_data_array.append(parsed_datum)
-
-    for each in parsed_data_array:
-        print('-------------')
-        print(each)
-
-    return parsed_data_array
-
-
 def construct_data_array_new_again():
 
     # should be in the form: array = [{day: date, wealth: [1, 2], health}]
@@ -123,7 +59,6 @@ def construct_data_array_new_again():
         print(each)
 
     return parsed_data_array
-
 
 
 if __name__ == "__main__":
--- main/models.py
+++ main/models.py
@@ -9,24 +9,6 @@ class Day(models.Model):
     number_events = models.IntegerField(default=0)
     notes = models.TextField(default='', blank=True, null=True)
 
-    # health:
-    # yoga = models.NullBooleanField(null=True, blank=True, default=None)
-    # work_out = models.NullBooleanField(null=True, blank=True, default=None)
-    # biking = models.NullBooleanField(null=True, blank=True, default=None)
-    # swimming = models.NullBooleanField(null=True, blank=True, default=None)
-    # limit_boozes = models.NullBooleanField(null=True, blank=True, default=None)
-    #
-    # # art:
-    # reading = models.NullBooleanField(null=True, blank=True, default=None)
-    # music = models.NullBooleanField(null=True, blank=True, default=None)
-    #
-    # # intellectually stimulating:
-    # coding = models.NullBooleanField(null=True, blank=True, default=None)
-    # podcast = models.NullBooleanField(null=True, blank=True, default=None)
-    #
-    # # finance:
-    # spend_limit_maintained = models.NullBooleanField(null=True, blank=True, default=None)
-
     def __unicode__(self):
         return 'day: {0}'.format(self.date)
 
