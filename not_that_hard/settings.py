3511bde24f48c88740d58cf686a45b4e683d1228
Jake Madison <jakemadison@gmail.com>
2015-02-02 11:39:15 -0800

    switch to test DB.

--- not_that_hard/settings.py
+++ not_that_hard/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'not_that_hard.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
     }
 }
 
